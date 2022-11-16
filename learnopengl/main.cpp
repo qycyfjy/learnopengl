0a93fb9d39abfb323d6e856b69116c82bf96ad3e
kyaru <kyaruny@gmail.com>
2022-11-17 05:41:16 +0800

    坐标系统与3d

--- learnopengl/main.cpp
+++ learnopengl/main.cpp
@@ -3,12 +3,22 @@
 
 #include "Shader.h"
 
+extern float vertices[];
+extern int vertices_size;
+extern glm::vec3 cube_positions[];
+extern int cube_positions_size;
+
 #if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
 #pragma comment(lib, "legacy_stdio_definitions")
 #endif
 
+int g_width = 800;
+int g_height = 600;
+
 void OnWindowResize(GLFWwindow* window, int width, int height) {
 	spdlog::info("current window size: {}x{}\n", width, height);
+	g_width = width;
+	g_height = height;
 	glViewport(0, 0, width, height);
 }
 
@@ -27,7 +37,7 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	auto window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+	auto window = glfwCreateWindow(g_width, g_height, "LearnOpenGL", nullptr, nullptr);
 	if (window == nullptr) {
 		printf("failed to create GLFW window\n");
 		glfwTerminate();
@@ -41,7 +51,8 @@ int main() {
 		printf("failed to init GLAD\n");
 		return -1;
 	}
-	glViewport(0, 0, 800, 600);
+	glEnable(GL_DEPTH_TEST);
+	glViewport(0, 0, g_width, g_height);
 
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
@@ -53,77 +64,46 @@ int main() {
 	stbi_set_flip_vertically_on_load(true);
 #pragma endregion
 
-	float vertices[] = {
-		0.5f,  0.5f, 0.0f,    1.0f, 0.0f, 0.0f,   1.0f, 1.0f,    // 右上
-		0.5f, -0.5f, 0.0f,    0.0f, 1.0f, 0.0f,   1.0f, 0.0f,    // 右下
-		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // 左下
-		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // 左上
-	};
-
-	unsigned int indices[] = {
-		0,1,2,
-		0,3,2,
-	};
-
 #pragma region Vertex, Texture, etc...
 	unsigned int VAO;
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
-	unsigned int VEO;
-	glGenBuffers(1, &VEO);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, VEO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	unsigned int VBO;
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glBufferData(GL_ARRAY_BUFFER, vertices_size, vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-	glEnableVertexAttribArray(2);
 
-	int w, h, nc;
-	unsigned char* data = stbi_load("rc/a.jpg", &w, &h, &nc, 0);
 	unsigned int texture1;
 	glGenTextures(1, &texture1);
-	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, texture1);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-	glGenerateMipmap(GL_TEXTURE_2D);
-	stbi_image_free(data);
-
-	data = stbi_load("rc/b.jpg", &w, &h, &nc, 0);
-	unsigned int texture2;
-	glGenTextures(1, &texture2);
-	glActiveTexture(GL_TEXTURE1);
-	glBindTexture(GL_TEXTURE_2D, texture2);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-	glGenerateMipmap(GL_TEXTURE_2D);
+	int w, h, nc;
+	unsigned char* data = stbi_load("rc/b.jpg", &w, &h, &nc, 0);
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else {
+		spdlog::error("failed to load texture");
+	}
 	stbi_image_free(data);
 #pragma endregion
 
 	Shader s("rc/vertex.txt", "rc/fragment.txt");
 	s.use();
 	s.setUniform("texture1", 0);
-	s.setUniform("texture2", 1);
 
-	float mix = 0.0f;
-	float scale = 0.5;
-	float rotate = 90.0;
-	bool auto_rotate = false;
-	float auto_rotate_speed = 90.0f;
-	float move_v = 0.0f;
-	float move_h = 0.0f;
+	float fov = 45.0f;
+	float ratio = g_width / static_cast<float>(g_height);
+	float near_plane = 0.1f;
+	float far_plane = 100.0f;
 
 #pragma region Loop Begin
 	while (!glfwWindowShouldClose(window)) {
@@ -131,44 +111,37 @@ int main() {
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
+
+		glClearColor(19 / 255.0f, 167 / 255.0f, 139 / 255.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		ImGui::Text("%.1f FPS", io.Framerate);
 #pragma endregion
 
 		///
 		/// 主逻辑
 		/// 
-		ImGui::SliderFloat("mixv", &mix, 0.0f, 1.0f);
-		s.setUniform("mixv", mix);
+		ImGui::SliderFloat("fov", &fov, 15.0f, 180.0f);
+		ImGui::SliderFloat("ratio", &ratio, 0.5f, 5.0f);
+		ImGui::SliderFloat("near plane", &near_plane, 0.01f, 10.0f);
+		ImGui::SliderFloat("far plane", &far_plane, 10.0f, 500.0f);
+		glm::mat4 projection = glm::mat4(1.0f);
+		projection = glm::perspective(glm::radians(fov), ratio, near_plane, far_plane);
+		s.setUniform("projection", projection);
 
-		// 缩放
-		glm::mat4 trans(1.0f);
-		ImGui::SliderFloat("scale", &scale, 0.0f, 10.0f);
-		trans = glm::scale(trans, glm::vec3(scale, scale, scale));
-
-		// 先位移
-		ImGui::SliderFloat("Move(V)", &move_v, -3.0f, 3.0f);
-		ImGui::SliderFloat("Move(H)", &move_h, -3.0f, 3.0f);
-		trans = glm::translate(trans, glm::vec3(move_v, move_h, 1.0f));
-
-		// 后旋转
-		ImGui::Checkbox("Auto Rotate", &auto_rotate);
-		if (auto_rotate) {
-			ImGui::SliderFloat("Speed", &auto_rotate_speed, 1.0f, 7200.0f);
-			rotate = fmodf(rotate + ImGui::GetIO().DeltaTime * auto_rotate_speed, 360.0f);
-		}
-		else {
-			ImGui::SliderFloat("rotate", &rotate, 0.0f, 360.0f);
-		}
-		trans = glm::rotate(trans, glm::radians(rotate), glm::vec3(0.0, 0.0, 1.0));
-
-		s.setUniform("transform", trans);
-
-		// RENDER
-		glClearColor(19 / 255.0f, 167 / 255.0f, 139 / 255.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-
+		glm::mat4 view = glm::mat4(1.0f);
+		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+		s.setUniform("view", view);
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		for (unsigned int i = 0; i < 10; i++)
+		{
+			glm::mat4 model(1.0f);
+			model = glm::translate(model, cube_positions[i]);
+			float angle = 20.0f * i + 10;
+			model = glm::rotate(model, (float)glfwGetTime() * glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			s.setUniform("model", model);
 
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 #pragma region Loop End
 		ImGui::Render();
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
